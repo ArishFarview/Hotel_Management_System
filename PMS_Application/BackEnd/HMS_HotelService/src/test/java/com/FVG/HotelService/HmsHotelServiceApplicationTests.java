3b0f09f4be3322f428eebeab6ee2f91f27ab1f03
Arish2242 <arishbabujagarlamudi@farviewglobal.com>
2025-06-23 16:35:56 +0530

    project structure

---
+++ PMS_Application/BackEnd/HMS_HotelService/src/test/java/com/FVG/HotelService/HmsHotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FVG.HotelService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsHotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
