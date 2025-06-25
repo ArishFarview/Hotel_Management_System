85b0e411c938b687257569e8b4eeef0b7ebf6843
Arish2242 <arishbabujagarlamudi@farviewglobal.com>
2025-06-26 00:51:27 +0530

    modules  integration done

--- Hotel-T2/Hotels_UI/src/App.jsx
+++ Hotel-T2/Hotels_UI/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,16 +7,28 @@ import Listing from './modules/Listing';
 import HotelDetails from './modules/HotelDetails';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [filterData,setfilterData] = useState({})
+
+  useEffect(() => {
+  const receiveMessage = (event) => {
+    if (event.origin !== "http://localhost:3000") { 
+      console.log("error");
+    } // security check
+    const hotelData = event.data;
+    console.log("Received hotel data:", hotelData);
+    setfilterData(hotelData)
+    // You can store in state, render results, etc.
+  };
+
+  window.addEventListener("message", receiveMessage);
+  return () => window.removeEventListener("message", receiveMessage);
+}, []);
 
   return (
     <div className="App">
-      {/* Micro Frontend: Search */}
-      <Search />
-      {/* Micro Frontend: Listing */}
-      <Listing />
-      {/* Micro Frontend: Hotel Details */}
-      <HotelDetails />
+     {filterData &&(
+      <h2>data is coming to the filterdata state </h2>
+     )}
     </div>
   )
 }
