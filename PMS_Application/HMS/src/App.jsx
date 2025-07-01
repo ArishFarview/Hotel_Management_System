cea8e2cff8738a659931eaeab4441c02ce194ed8
Viswagna Brahma Teki <viswagnabrahmateki@farviewglobal.com>
2025-07-01 15:05:50 +0530

    removed hardcoded

--- PMS_Application/HMS/src/App.jsx
+++ PMS_Application/HMS/src/App.jsx
@@ -1,3 +1,37 @@
+// import { useEffect, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import Routings from './components/Routings'
+// import Header from './components/Header'
+
+// function App() {
+//   useEffect(() => {
+//   const handleMessage = (event) => {
+//     if (event.origin !== "http://13.127.177.129:3001") return;
+//     console.log("Received hotel data:", event);
+//     // Use it to populate a booking form, or send to backend
+//     // console.log(event);
+    
+//   };
+
+//   window.addEventListener("message", handleMessage);
+//   return () => window.removeEventListener("message", handleMessage);
+// }, []);
+
+
+
+//   return (
+//     <>
+//      <Header />
+//      <Routings />
+      
+//     </>
+//   )
+// }
+
+// export default App
+
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,7 +42,7 @@ import Header from './components/Header'
 function App() {
   useEffect(() => {
   const handleMessage = (event) => {
-    if (event.origin !== "http://13.127.177.129:3001") return;
+    if (event.origin !== "/train/") return;
     console.log("Received hotel data:", event);
     // Use it to populate a booking form, or send to backend
     // console.log(event);
